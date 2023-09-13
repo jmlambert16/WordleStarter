002ddb9c0f0d8791b3541ba6c4245aef73fb8427
elebaro2 <33472380+elebaro2@users.noreply.github.com>
2023-09-13 08:11:25 -0600

    Check if word in list

--- Wordle.py
+++ Wordle.py
@@ -11,22 +11,38 @@ from WordleDictionary import FIVE_LETTER_WORDS
 from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
 
 def wordle():
+    #Check if word is from dictionary
+    def check_word(s):
+        word = ""
+        for col, letter in enumerate(Milestone_1):
+            row = 0
+            letter = gw.get_square_letter(row, col)
+            word = word + letter
+        lowercase = word.lower()
 
-    def enter_action(s):
-        gw.show_message("You have to implement this method.")
+        if lowercase in FIVE_LETTER_WORDS:
+            gw.show_message("Yep")
+        else:
+            gw.show_message("Not in word list")
 
     gw = WordleGWindow()
-    gw.add_enter_listener(enter_action)
+    gw.add_enter_listener(check_word)
+    
 
     # Milestone_1
     Milestone_1 = random.choice(FIVE_LETTER_WORDS)
     for col, letter in enumerate(Milestone_1):
         row = 0
         gw.set_square_letter(row, col, letter)
+
+
+
+
+
+
         
 
 # Startup code
 
 if __name__ == "__main__":
     wordle()
-
