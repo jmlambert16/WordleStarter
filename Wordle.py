de37a363b491289776a03e31fbaad357f29009ce
Eliot Amado <eamado@byu.edu>
2023-09-20 15:37:37 -0600

    Green if correct, grey if not in right place, red

--- Wordle.py
+++ Wordle.py
@@ -6,36 +6,38 @@ BE SURE TO UPDATE THIS COMMENT WHEN YOU WRITE THE CODE.
 """
 
 import random
-
 from WordleDictionary import FIVE_LETTER_WORDS
-from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
+from WordleGraphics import WordleGWindow, N_COLS, N_ROWS, CORRECT_COLOR, MISSING_COLOR
 
 def wordle():
-    #Check if word is from dictionary
+    # Check if word is from dictionary
     def check_word(s):
-        word = ""
-        row = gw.get_current_row() #sets current row 
-        for col, letter in enumerate(Milestone_1):
-            letter = gw.get_square_letter(row, col)
-            word = word + letter
-        lowercase = word.lower()
+        guess = ""
+        row = gw.get_current_row()  # Get current row
+        hidden_word = Milestone_1.lower()  # Convert the hidden word to lowercase for comparison
 
-        if lowercase in FIVE_LETTER_WORDS:
-            gw.show_message("Yep")
-            
-            #if word is dictionary then it changes green
-            for col, letter in enumerate(Milestone_1):
-                gw.set_square_color(row, col, 'green')
-        else:
-            gw.show_message("Not in word list")
-            #if word is not in dictionary then it changes red
-            for col, letter in enumerate(Milestone_1):
+        for col in range(N_COLS):
+            letter = gw.get_square_letter(row, col)
+            guess += letter
+
+            if letter.lower() == hidden_word[col]:
+                # Correct letter in correct position
+                gw.set_square_color(row, col, CORRECT_COLOR)
+            elif letter.lower() in hidden_word:
+                # Letter is in the word but not in the correct position
+                gw.set_square_color(row, col, MISSING_COLOR)
+            else:
+                # Incorrect letter
                 gw.set_square_color(row, col, 'red')
 
-        #Increments current row by 1 each time enter is pressed
+        if guess.lower() == hidden_word:
+            gw.show_message("Congratulations! You guessed the word.")
+        else:
+            gw.show_message("Keep trying!")
+
+        # Increment current row by 1 each time enter is pressed
         row += 1
         gw.set_current_row(row)
-
 
     gw = WordleGWindow()
     gw.add_enter_listener(check_word)
@@ -46,14 +48,6 @@ def wordle():
         row = 0
         gw.set_square_letter(row, col, letter)
 
-
-
-
-
-
-        
-
 # Startup code
-
 if __name__ == "__main__":
     wordle()
