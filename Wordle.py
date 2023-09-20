22d16f5cb43d64cfe8dc8c9acac70cf477e5ba42
elebaro2 <33472380+elebaro2@users.noreply.github.com>
2023-09-20 17:29:32 -0600

    Toggle colors

--- Wordle.py
+++ Wordle.py
@@ -1,17 +1,15 @@
-# File: Wordle.py
-
-"""
-This module is the starter file for the Wordle assignment.
-BE SURE TO UPDATE THIS COMMENT WHEN YOU WRITE THE CODE.
-"""
-
 import random
 from WordleDictionary import FIVE_LETTER_WORDS
 from WordleGraphics import WordleGWindow, N_COLS, N_ROWS, CORRECT_COLOR, MISSING_COLOR
+import tkinter as tk
+import threading
 
 def wordle():
+    grayscale_mode = False  # Variable to track grayscale mode
+
     # Check if word is from dictionary
     def check_word(s):
+        nonlocal grayscale_mode  # Access the outer grayscale_mode variable
         guess = ""
         row = gw.get_current_row()  # Get current row
         hidden_word = Milestone_1.lower()  # Convert the hidden word to lowercase for comparison
@@ -22,13 +20,22 @@ def wordle():
 
             if letter.lower() == hidden_word[col]:
                 # Correct letter in correct position
-                gw.set_square_color(row, col, CORRECT_COLOR)
+                if grayscale_mode:
+                    gw.set_square_color(row, col, 'gray1')
+                else:
+                    gw.set_square_color(row, col, CORRECT_COLOR)
             elif letter.lower() in hidden_word:
                 # Letter is in the word but not in the correct position
-                gw.set_square_color(row, col, MISSING_COLOR)
+                if grayscale_mode:
+                    gw.set_square_color(row, col, 'gray')
+                else:
+                    gw.set_square_color(row, col, MISSING_COLOR)
             else:
-                # Incorrect letter
-                gw.set_square_color(row, col, 'red')
+                if grayscale_mode:
+                    gw.set_square_color(row, col, 'gray89')
+                else:
+                    # Incorrect letter
+                    gw.set_square_color(row, col, 'red')
 
         if guess.lower() == hidden_word:
             gw.show_message("Congratulations! You guessed the word.")
@@ -39,6 +46,17 @@ def wordle():
         row += 1
         gw.set_current_row(row)
 
+    def toggle_grayscale_mode():
+        nonlocal grayscale_mode  
+        grayscale_mode = not grayscale_mode  
+
+    def toggle_grayscale_mode_button():
+        toggle_grayscale_mode()
+        if grayscale_mode:
+            grayscale_button.config(text="Grayscale Mode ON")
+        else:
+            grayscale_button.config(text="Grayscale Mode OFF")
+
     gw = WordleGWindow()
     gw.add_enter_listener(check_word)
 
@@ -48,6 +66,16 @@ def wordle():
         row = 0
         gw.set_square_letter(row, col, letter)
 
-# Startup code
+    # Create a  window for the button
+    root = tk.Tk()
+    root.title("Grayscale Mode Toggle")
+
+    # Create a button to toggle grayscale mode
+    grayscale_button = tk.Button(root, text="Grayscale Mode OFF", command=toggle_grayscale_mode_button)
+    grayscale_button.pack(pady=10)
+
+    threading.Thread(target=root.mainloop).start()
+
+# Start the Wordle game in the main thread
 if __name__ == "__main__":
     wordle()
